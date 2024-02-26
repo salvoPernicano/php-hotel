c146a90425cd2d10d19b18543aca61d7ed14bf07
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-26 21:04:48 +0100

    feat stile/stampa bootstrap

--- index.php
+++ index.php
@@ -40,12 +40,6 @@
 
     ];
 
-    foreach ($hotels as $hotel){
-        foreach ($hotel as $feature => $value){
-            echo "$feature - $value . <br>";
-        }
-        echo "<---><br>";
-    };
 ?>
 
 <!DOCTYPE html>
@@ -53,9 +47,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    
+<table class="container table table-secondary">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance To Center</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <?php
+     foreach ($hotels as $hotel){
+        echo "<tr>";
+        foreach ($hotel as $feature => $value){
+            echo "<td>" . $value . "</td>";
+        }
+        echo "</tr>";
+    };
+    ?>
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
